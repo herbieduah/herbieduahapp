62ba27f8d57a6a7055d079592156b7fef9704364
Herbie Duah <herbduah@gmail.com>
2019-09-20 23:36:53 -0700

    react scripts updated

--- src/index.js
+++ src/index.js
@@ -21,4 +21,4 @@ ReactDOM.render(<Main />, rootElement);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
